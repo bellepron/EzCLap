4abe18c99d8a6144327f637d145340dc9d6c256a
caglar <caglarkutluyigit@gmail.com>
2021-11-25 22:12:32 +0300

    refactoring: handmove

--- BoatStack/Assets/AAA/0 Scripts/UI/HandMove.cs
+++ BoatStack/Assets/AAA/0 Scripts/UI/HandMove.cs
@@ -8,12 +8,6 @@ public class HandMove : MonoBehaviour
     float firstPos;
     void Start()
     {
-        firstPos = transform.position.x;
-        Move();
-    }
-    void Move()
-    {
-        transform.position = new Vector2(firstPos, transform.position.y);
-        transform.DOMoveX(transform.position.x + 500, 1.5f).OnComplete(() => Move());
+        transform.DOMoveX(transform.position.x + 500, 1f).SetLoops(-1);
     }
 }
