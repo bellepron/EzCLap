91cffe9cb04f45ad066f7411b96ec72b00d823e3
caglar <caglarkutluyigit@gmail.com>
2021-11-29 01:10:02 +0300

    level end part

--- BoatStack/Assets/AAA/0 Scripts/CameraManager.cs
+++ BoatStack/Assets/AAA/0 Scripts/CameraManager.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-public class CameraManager : MonoBehaviour, IWinObserver
+public class CameraManager : MonoBehaviour, IWinObserver, ILevelEndObserver
 {
     public static CameraManager Instance;
 
     [SerializeField] CinemachineVirtualCamera v_Cam1;
     [SerializeField] CinemachineVirtualCamera v_Cam2;
+    [SerializeField] CinemachineVirtualCamera v_Cam3;
 
     private void Awake()
     {
@@ -18,13 +19,11 @@ public class CameraManager : MonoBehaviour, IWinObserver
     private void Start()
     {
         Observers.Instance.Add_WinObserver(this);
-        Cam2_Cam1();
-    }
+        Observers.Instance.Add_LevelEndObserver(this);
 
-    public void Cam2_Cam1()
-    {
         v_Cam1.Priority = 10;
         v_Cam2.Priority = 0;
+        v_Cam3.Priority = 0;
     }
 
     public void Cam1_Cam2()
@@ -32,9 +31,19 @@ public class CameraManager : MonoBehaviour, IWinObserver
         v_Cam1.Priority = 0;
         v_Cam2.Priority = 10;
     }
+    public void Cam2_Cam3()
+    {
+        v_Cam2.Priority = 0;
+        v_Cam3.Priority = 10;
+    }
 
     public void WinScenario()
     {
         Cam1_Cam2();
+    }
+
+    public void LevelEnd()
+    {
+        Cam2_Cam3();
     }
 }
