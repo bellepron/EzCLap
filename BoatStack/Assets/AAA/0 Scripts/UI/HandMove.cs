385fdd0a89fee15b327bfd5646e14d17aeeb4ac1
caglar <caglarkutluyigit@gmail.com>
2021-12-05 19:24:17 +0300

    Update HandMove.cs

--- BoatStack/Assets/AAA/0 Scripts/UI/HandMove.cs
+++ BoatStack/Assets/AAA/0 Scripts/UI/HandMove.cs
@@ -8,6 +8,6 @@ public class HandMove : MonoBehaviour
     float firstPos;
     void Start()
     {
-        transform.DOMoveX(transform.position.x + 500, 1f).SetLoops(-1);
+        transform.DOMoveX(transform.position.x + 400, 1f).SetLoops(-1);
     }
 }
