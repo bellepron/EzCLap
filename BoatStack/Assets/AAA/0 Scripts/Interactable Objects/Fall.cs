1c9bccb27d2c334d894f8f16dcde3f411c9485da
caglar <caglarkutluyigit@gmail.com>
2021-12-05 19:23:04 +0300

    update prefabs

--- BoatStack/Assets/AAA/0 Scripts/Interactable Objects/Fall.cs
+++ BoatStack/Assets/AAA/0 Scripts/Interactable Objects/Fall.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class Fall : MonoBehaviour, IInteractable
 {
+    [SerializeField] BoxCollider boxCollider;
     public void Interact()
     {
         Player.Instance.Fall();
+        boxCollider.enabled = false;
     }
 }
